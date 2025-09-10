d1c4cfd13075d0ba4ccbe6bd02aa712ba54c4db7
Igor Yavkin <last85@list.ru>
2025-09-10 05:11:00 +0300

    Added string test

---
+++ modbus_sources/modbus_string_test.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <modbus/modbus.h>
+#include <unistd.h>
+#include <time.h>
+#include <wchar.h>
+#include <locale.h>
+#include <errno.h>
+
+#define STRING_REGISTER_START 4208
+#define STRING_LENGTH_REGISTER 4249
+#define MAX_STRING_LENGTH 256
+#define REGISTERS_PER_CHAR 1
+
+int send_string(modbus_t * modbus_context, const char* str)
+{
+    int text_len=strlen(str);
+    int registers_needed=(text_len+1)/2;
+    if (registers_needed>128)
+    {
+        fprintf(stderr,"Строка слишком длинная \n");
+        return 1;
+    }
+
+    u_int16_t string_data[]={0};
+
+    for (int i=0;i<text_len;i=+2)
+    {
+        u_int16_t reg_value=str[i];
+        if (i+1<text_len){
+            reg_value|=(str[i+1]<<8);
+        }
+        string_data[i/2]=reg_value;
+    }
+
+      // Устанавливаем длину строки
+    if (modbus_write_register(modbus_context, STRING_LENGTH_REGISTER, text_len) == -1) {
+        fprintf(stderr, "❌ Ошибка записи длины строки\n");
+        return 1;
+    }
+
+    // Записываем саму строку
+    if (modbus_write_registers(modbus_context, STRING_REGISTER_START, registers_needed, string_data) == -1) {
+        fprintf(stderr, "❌ Ошибка записи строки\n");
+        return 1;
+    }
+
+}
+
+
+void test_basic_strings(modbus_t *modbus_context)
+{
+    printf("Тест базовых строк \n");
+    printf("===================\n");
+
+    const char* strings[]={
+        "Hello world",
+        "Текст на русском",
+        "1234567890",
+        NULL
+    };
+
+    for (int i=0;strings[i]!=NULL;i++)
+    {
+        send_string(modbus_context,strings[i]);
+    }
+
+
+
+}
+
+
+int main(int argc,char** argv)
+{
+    const char* port="/dev/ttyUSB0";
+    int baud = 115200;
+    int slave_id = 1;
+
+
+    printf("\033[32m\nТест отправки  строк на Modbus индикатор Овен СМИ2-М\033[0m\n");
+
+    modbus_t  *modbus= modbus_new_rtu(port,baud,'N',1,1);
+
+    if (modbus==NULL){
+        fprintf(stderr,"Не удалось создать контекст Модбас устройства: %s \n", modbus_strerror(errno));
+        return 1;
+    }
+
+    modbus_set_slave(modbus,1);
+    modbus_set_response_timeout(modbus,1,0);
+
+    if (modbus_connect(modbus)==-1)
+    {
+        fprintf(stderr,"Не получилось установить Modbus соединение: %s \n",modbus_strerror(errno));
+        modbus_free(modbus);
+        return 1;
+    }
+
+    printf("\033[32mПодключение к СМИ2-М установлено \033[0m\n");
+
+    test_basic_strings(modbus);
+
+    modbus_close(modbus);
+    modbus_free(modbus);
+
+    return 0;
+}
