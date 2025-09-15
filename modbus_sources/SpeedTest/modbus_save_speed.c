89129073f4b3b687971c9865135ee94ce81e8b06
Igor Yavkin <last85@list.ru>
2025-09-15 22:50:29 +0300

    speed_test: Доработал логику установки и проверки скоростей
    Signed-off-by: Igor Yavkin <last85@list.ru>

--- modbus_sources/SpeedTest/modbus_save_speed.c
+++ modbus_sources/SpeedTest/modbus_save_speed.c
@@ -7,10 +7,10 @@
 
 
 //Определяем регистры для записи настроек
-#define SPEED_REGISTER 521
+#define SAVE_REGISTER 5000
 
 
-#define CON_SPEED 4800
+#define CON_SPEED 115200
 #define CON_STOPBITS 1
 #define CON_PARITY 'N'
 #define CON_DATABITS 8
@@ -18,8 +18,7 @@
 
 int main(int argc, char** argv)
 {
-    int now_speed=115200;
-    int set_speed=10;
+    int speed=10;
     int data_bits=0;
     int stop_bits=0;
     int parity=0;
@@ -28,27 +27,26 @@ int main(int argc, char** argv)
 
     const char* port="/dev/ttyUSB0";
 
-    if (argc!=3)
+    if (argc!=2)
     {
-        printf("\033[32mИспользование %s: <set_speed> <now_speed> \033[0m\n\
+        printf("\033[32mИспользование %s:\033[0m\n\
             Скорость:\n\
-            3 - 2400 бит\\c\n\
-            4 - 4800 бит\\c\n\
-            5 - 9600 бит\\c\n\
-            6 - 14400 бит\\c\n\
-            7 - 19200 бит\\c\n\
-            8 - 38400 бит\\c\n\
-            9 - 57600 бит\\c\n\
-            10 - 115200 бит\\c\n",argv[0]);
+             2400 бит\\c\n\
+             4800 бит\\c\n\
+             9600 бит\\c\n\
+             14400 бит\\c\n\
+             19200 бит\\c\n\
+             38400 бит\\c\n\
+             57600 бит\\c\n\
+             115200 бит\\c\n",argv[0]);
     }
     else
     {
-        set_speed=atoi(argv[1]);
-        now_speed=atoi(argv[2]);
+        speed=atoi(argv[1]);
     }
 
     // Создаем контекст модбас
-    modbus=modbus_new_rtu(port,now_speed,CON_PARITY,CON_DATABITS,CON_STOPBITS);
+    modbus=modbus_new_rtu(port,CON_SPEED,CON_PARITY,CON_DATABITS,CON_STOPBITS);
     if (modbus==NULL)
     {
             fprintf(stderr,"\033[31mОшибка создания модбас контекста\n\033[0m");
@@ -70,7 +68,7 @@ int main(int argc, char** argv)
 
     printf("Соединение установлено\n");
 
-    if (modbus_write_register(modbus,SPEED_REGISTER,set_speed)==-1)
+    if (modbus_write_register(modbus,SAVE_REGISTER,speed)==-1)
     {
         fprintf(stderr,"Ошибка записи параметра скорости: %s \n",modbus_strerror(errno));
         modbus_close(modbus);
--- modbus_sources/SpeedTest/modbus_speed_test.c
+++ modbus_sources/SpeedTest/modbus_speed_test.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define STATUS_REGISTER 4100
+#define STATUS_REGISTER 521
 
 int main(int argc, char **argv)
 {
@@ -58,7 +58,35 @@ int main(int argc, char **argv)
         return -1;
     }
     
-    printf("Регистр успешно прочитан : 0x%04X\n",status);
+    printf("Регистр cкорости успешно прочитан:\n");
+
+    switch (status)
+    {
+    case 0x03:
+        printf("\033[32mУстановлена скорость 2400\033[0m\n");
+        break;
+    case 0x04:
+        printf("\033[32mУстановлена скорость 4800\033[0m\n");
+        break;
+    case 0x05:
+        printf("\033[32mУстановлена скорость 9600\033[0m\n");
+        break;
+    case 0x06:
+        printf("\033[32mУстановлена скорость 14400\033[0m\n");
+        break;
+    case 0x07:
+        printf("\033[32mУстановлена скорость 19200\033[0m\n");
+        break;
+    case 0x08:
+        printf("\033[32mУстановлена скорость 38400\033[0m\n");
+        break;
+    case 0x09:
+        printf("\033[32mУстановлена скорость 57600\033[0m\n");
+        break;    
+    case 0x0A:
+        printf("\033[32mУстановлена скорость 115200\033[0m\n");
+        break;
+    }
 
     modbus_close(modbus);
     modbus_free(modbus);
